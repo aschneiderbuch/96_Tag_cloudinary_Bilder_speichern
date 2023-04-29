eb3d5b0a7457d6bbb06d146265acb8b6f9e8779e
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-04-29 14:34:19 +0200

    fE cloudinary Route für hochladenZuBackEnd

--- frontEnd/wa-create-react-app/src/App.jsx
+++ frontEnd/wa-create-react-app/src/App.jsx
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const imgRef = useRef()
+  const [images, setImages] = useState([])
+
+  const hochladenZuBackEnd = async () => {
+    const form = new FormData();
+    form.append('file', imgRef.current.files[0])
+    form.append('name', 'nochEinBild')
+
+    const response = await fetch('http://localhost:9999/directupload', {
+      method: 'POST',
+      body: form
+    })
+
+    const data = await response.json()
+    setImages(prev => [...prev, data.url])
+  }
+
+
+
   return (
-    <div className="App">
-{/*       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-    </div>
+    <section className="App">
+
+      <article>
+        <label htmlFor="file">Datei zum Hochladen</label>
+        <input ref={imgRef} type="file" name="file" id="file" placeholder='max 2 MB' />
+        <button onClick={hochladenZuBackEnd}>Hochladen zu BackEnd</button>
+      </article>
+
+      {
+        images.map((img, key) => {
+          return <img style={{ width: '25%' }} key={key} src={img} alt='hochgeladenes Bild' > </img>
+        })
+      }
+
+    </section>
   );
 }
 
